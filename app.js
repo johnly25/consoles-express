4e1f9da550a15cd564b0902a0a989bdf004d1af0
Johnny <nguyen.johnly25>
2024-08-04 16:39:46 -0700

    remove dev url

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const limiter = RateLimit({
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
